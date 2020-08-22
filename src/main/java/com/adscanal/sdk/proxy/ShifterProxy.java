0a672e3add11b478623235a02e1670d1cbef776e
huangyongchao <huangyongchao@emar.com>
2020-08-23 00:13:10 +0800

    little

--- src/main/java/com/adscanal/sdk/proxy/ShifterProxy.java
+++ src/main/java/com/adscanal/sdk/proxy/ShifterProxy.java
@@ -200,7 +200,7 @@ public class ShifterProxy {
 
 
         try {
-            System.setProperty("java.util.concurrent.ForkJoinPool.common.parallelism", "2000");
+            System.setProperty("java.util.concurrent.ForkJoinPool.common.parallelism", "100");
 
             Files.lines(Paths.get("/opt/did/VNMios.log.dist")).parallel().forEach(deviceid -> {
                 int i = at_req.getAndAdd(1);
