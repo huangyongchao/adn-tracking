dbbf8d4afd719dd70dc80a9121a6c1a704cd6597
huangyongchao <huangyongchao@emar.com>
2020-10-27 22:58:20 +0800

    little

--- src/main/java/com/adscanal/sdk/core/OfferTask.java
+++ src/main/java/com/adscanal/sdk/core/OfferTask.java
@@ -131,7 +131,7 @@ public class OfferTask implements Runnable {
             SimpleData.PRODUCERCOUNTER.get(key).getRequest().incrementAndGet();
         } catch (Exception e) {
             Counter.increaseError(offer.getUid());
-            errorlog.error(e.getMessage(), offer.getUid()+" "+offer.getId()+"\n"+e);
+            errorlog.error(e.getMessage(),e);
             error_req_account.incrementAndGet();
         }
 
