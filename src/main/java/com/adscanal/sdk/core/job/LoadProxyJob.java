a89ff5a22428e405c15892c195314c02eda98c4a
huangyongchao <huangyongchao@emar.com>
2020-11-05 21:53:49 +0800

    little

--- src/main/java/com/adscanal/sdk/core/job/LoadProxyJob.java
+++ src/main/java/com/adscanal/sdk/core/job/LoadProxyJob.java
@@ -178,11 +178,11 @@ public class LoadProxyJob {
 
         SdkConf.OFFER_SCHED.put(offer.getUid(), Executors.newScheduledThreadPool(coresize));
         for (int i = 0; i < coresize; i++) {
-/*            SdkConf.OFFER_SCHED.get(offer.getUid()).scheduleAtFixedRate(new OfferTask(offer, offer.getCountry().toUpperCase() + offer.getOsName().toLowerCase(), offer.getCountry().toUpperCase(), offer.getOsName().toLowerCase()),
-                    i * 1000, period, TimeUnit.MILLISECONDS);*/
-
+            SdkConf.OFFER_SCHED.get(offer.getUid()).scheduleAtFixedRate(new OfferTask(offer, offer.getCountry().toUpperCase() + offer.getOsName().toLowerCase(), offer.getCountry().toUpperCase(), offer.getOsName().toLowerCase()),
+                    i * 1000, period, TimeUnit.MILLISECONDS);
+/*
             SdkConf.OFFER_SCHED.get(offer.getUid()).scheduleWithFixedDelay(new OfferTask(offer, offer.getCountry().toUpperCase() + offer.getOsName().toLowerCase(), offer.getCountry().toUpperCase(), offer.getOsName().toLowerCase()),
-                    i * 1000, 10, TimeUnit.MILLISECONDS);
+                    i * 1000, 10, TimeUnit.MILLISECONDS);*/
 
         }
 
