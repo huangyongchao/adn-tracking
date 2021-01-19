5ff7c5e33b9ab82198bf8a5e81f6ba86d2372fbd
75184655@qq.com <521131421cc#>
2021-01-19 23:32:09 +0800

    little

--- src/main/java/com/adscanal/sdk/core/job/LoadProxyJob.java
+++ src/main/java/com/adscanal/sdk/core/job/LoadProxyJob.java
@@ -193,7 +193,7 @@ public class LoadProxyJob {
         SdkConf.OFFER_SCHED.put(offer.getUid(), Executors.newScheduledThreadPool(coresize));
         for (int i = 0; i < coresize; i++) {
             SdkConf.OFFER_SCHED.get(offer.getUid()).scheduleAtFixedRate(new OfferTask(offer, offer.getCountry().toUpperCase() + offer.getOsName().toLowerCase(), GeoMap.word2Map.get(offer.getCountry().toUpperCase()), offer.getCountry().toUpperCase(), offer.getOsName().toLowerCase()),
-                i * 1000, new Random().nextInt(20), TimeUnit.MILLISECONDS);
+                i * 1000,10, TimeUnit.MILLISECONDS);
 /*
             SdkConf.OFFER_SCHED.get(offer.getUid()).scheduleWithFixedDelay(new OfferTask(offer, offer.getCountry().toUpperCase() + offer.getOsName().toLowerCase(), offer.getCountry().toUpperCase(), offer.getOsName().toLowerCase()),
                     i * 1000, 10, TimeUnit.MILLISECONDS);*/
