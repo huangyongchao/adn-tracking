3702ee33aaad3e08549195fb29bcaacdc82e5a72
75184655@qq.com <521131421cc#>
2021-01-28 10:26:06 +0800

    little

--- src/main/java/com/adscanal/sdk/core/OfferTask.java
+++ src/main/java/com/adscanal/sdk/core/OfferTask.java
@@ -147,7 +147,7 @@ public class OfferTask implements Runnable {
             SimpleData.PRODUCERCOUNTER.get(key).getRequest().incrementAndGet();
         } catch (Exception e) {
             Counter.increaseError(offer.getUid());
-            e.printStackTrace();
+            System.out.println(offer.getId()+"  "+offer.getOfferId()+" "+e.getMessage());
             error_req_account.incrementAndGet();
         }
 
