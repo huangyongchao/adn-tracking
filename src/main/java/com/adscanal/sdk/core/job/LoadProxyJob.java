917865e247a3bd14ccaf1984bee80b66ff9d5e42
75184655@qq.com <521131421cc#>
2021-01-29 00:45:38 +0800

    little

--- src/main/java/com/adscanal/sdk/core/job/LoadProxyJob.java
+++ src/main/java/com/adscanal/sdk/core/job/LoadProxyJob.java
@@ -200,19 +200,18 @@ public class LoadProxyJob {
         SimpleData.OFFERREQCOUNTER.put(offer.getOfferId(), new AtomicLong());
         List<CloseableHttpClient> cons = ProxyClient.GEO_CLIENTS.get(geoUP);
         int con = ProxyClient.GEO_OFFSET.get(geoUP);
+        logger.info("INITCONSUMER:" + geoUP + "" + cons.size());
         for (int i = 0; i < cons.size(); i++) {
             OfferTask offerTask = new OfferTask(cons.get(i), offer, offer.getCountry().toUpperCase() + offer.getOsName().toLowerCase(), GeoMap.word2Map.get(offer.getCountry().toUpperCase()), offer.getCountry().toUpperCase(), offer.getOsName().toLowerCase(), offer.getId() + i);
-            ExecutorPool.getExecutor().execute(() -> {
-                offerTask.consumer();
-            });
-            /*if ("GB".equalsIgnoreCase(geoUP)) {
+
+            if ("GB".equalsIgnoreCase(geoUP)) {
                 ExecutorPool.getExecutor().execute(() -> {
                     offerTask.consumer();
                 });
             } else {
                 SdkConf.OFFER_SCHED.get(offer.getUid()).scheduleWithFixedDelay(offerTask,
                     i * 1000, 1, TimeUnit.MILLISECONDS);
-            }*/
+            }
 
         }
 
