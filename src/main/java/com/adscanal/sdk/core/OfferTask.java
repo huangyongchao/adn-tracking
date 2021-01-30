4db336f7c7bc37b2eed8637b82b8dc0f69f4bd8d
75184655@qq.com <521131421cc#>
2021-01-30 13:46:10 +0800

    little

--- src/main/java/com/adscanal/sdk/core/OfferTask.java
+++ src/main/java/com/adscanal/sdk/core/OfferTask.java
@@ -64,7 +64,7 @@ public class OfferTask implements Runnable {
             String url = AdTool.trackurl(os, offer.getTrackUrl(), AdTool.randomSub(offer), deviceid, AdTool.geClickid(offer), null);
             String ua = AdTool.randomUA(os);
             //request(key, , url, ua, offer, null, deviceid, os);
-            //request(key, ProxyClient.getConn(geo, serNo), url, ua, offer, null, deviceid, os);
+            request(key, ProxyClient.getConn(geo, serNo), url, ua, offer, null, deviceid, os);
             at_req.incrementAndGet();
         } catch (InterruptedException e) {
             SimpleData.PRODUCERCOUNTER.get(key).getError().incrementAndGet();
