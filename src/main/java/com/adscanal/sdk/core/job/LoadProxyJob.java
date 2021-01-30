ec579e49c906234081818d92fedb1c4c6db0c869
75184655@qq.com <521131421cc#>
2021-01-30 15:21:21 +0800

    little

--- src/main/java/com/adscanal/sdk/core/job/LoadProxyJob.java
+++ src/main/java/com/adscanal/sdk/core/job/LoadProxyJob.java
@@ -184,18 +184,20 @@ public class LoadProxyJob {
         SimpleData.OFFERREQCOUNTER.put(offer.getOfferId(), new AtomicLong());
 
         SdkConf.OFFER_SCHED.put(offer.getUid(), Executors.newScheduledThreadPool(coresize));
-        for (int i = 0; i < ProxyClient.GEO_CLIENTS.get(geoUP).size(); i++) {
-            final int serNo = i;
-            OfferTask offerTask = new OfferTask(offer, offer.getCountry().toUpperCase() + offer.getOsName().toLowerCase(), GeoMap.word2Map.get(offer.getCountry().toUpperCase()), offer.getCountry().toUpperCase(), offer.getOsName().toLowerCase(), serNo);
+        for (int j = 0; j < coresize; j++) {
+            for (int i = 0; i < ProxyClient.GEO_CLIENTS.get(geoUP).size(); i++) {
+                final int serNo = i;
+                OfferTask offerTask = new OfferTask(offer, offer.getCountry().toUpperCase() + offer.getOsName().toLowerCase(), GeoMap.word2Map.get(offer.getCountry().toUpperCase()), offer.getCountry().toUpperCase(), offer.getOsName().toLowerCase(), serNo);
           /*      SdkConf.OFFER_SCHED.get(offer.getUid()).scheduleWithFixedDelay(offerTask,
                     i * 1000, 1, TimeUnit.MILLISECONDS);*/
 
-            ExecutorPool.getExecutor().execute(() -> {
-                offerTask.consumer(serNo);
+                ExecutorPool.getExecutor().execute(() -> {
+                    offerTask.consumer(serNo);
 
-            });
+                });
 
 
+            }
         }
 
 
