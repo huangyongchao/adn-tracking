952fcdc88850e0d03724e413772005ed52efd179
75184655@qq.com <521131421cc#>
2021-03-17 23:07:53 +0800

    little

--- src/main/java/com/adscanal/sdk/core/job/LoadProxyJob.java
+++ src/main/java/com/adscanal/sdk/core/job/LoadProxyJob.java
@@ -178,7 +178,7 @@ public class LoadProxyJob {
         if (priority > 5) {
             priority = 5;
         }
-        coresize = clicks / 50000;
+        coresize = clicks / 25000;
         if(coresize>50){
             coresize = 50;
         }
