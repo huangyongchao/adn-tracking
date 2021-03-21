30cf615aa6f90f4fe16dd909aff08b2ae9f32f8a
75184655@qq.com <521131421cc#>
2021-03-21 23:14:55 +0800

    little

--- src/main/java/com/adscanal/sdk/conf/EnableAPR.java
+++ src/main/java/com/adscanal/sdk/conf/EnableAPR.java
@@ -5,7 +5,7 @@ import org.springframework.boot.web.server.WebServerFactory;
 import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+//@Configuration
 public class EnableAPR implements WebServerFactoryCustomizer<WebServerFactory> {
 
     @Override
