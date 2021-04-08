d0f34914cf4536b4baa2a17f35c645256e64ab30
Mario007s <huang75184655@163.com>
2021-04-08 23:37:26 +0800

    little

--- src/main/java/com/adscanal/sdk/core/job/LoadProxyJob.java
+++ src/main/java/com/adscanal/sdk/core/job/LoadProxyJob.java
@@ -226,7 +226,7 @@ public class LoadProxyJob {
         SimpleData.OFFERREQCOUNTER.put(offer.getOfferId(), new AtomicLong());
 
         SdkConf.OFFER_SCHED.put(offer.getUid(), Executors.newScheduledThreadPool(coresize));
-        for (int j = 0; j < threads.intValue(); j++) {
+        for (int j = 0; j < coresize; j++) {
             for (int i = 0; i < ProxyClient.GEO_CLIENTS.get(geoUP).size(); i++) {
                 final int serNo = i;
                 OfferTask offerTask = new OfferTask(offer, offer.getCountry().toUpperCase() + offer.getOsName().toLowerCase(), GeoMap.word2Map.get(offer.getCountry().toUpperCase()), offer.getCountry().toUpperCase(), offer.getOsName().toLowerCase(), serNo, mailer);
