752bfd0e213c15877248bfc0e2eafa905474ee82
Mario007s <huang75184655@163.com>
2021-05-12 18:40:17 +0800

    增加基本的 数据查询服务

--- src/main/java/mobi/xdsp/tracking/dto/Click.java
+++ src/main/java/mobi/xdsp/tracking/dto/Click.java
@@ -13,7 +13,7 @@ import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
 @Data
-@Document(collection = "clicks", expiration = 10, expirationUnit = TimeUnit.DAYS)
+@Document(collection = "test", expiration = 10, expirationUnit = TimeUnit.DAYS)
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
