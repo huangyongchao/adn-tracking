da1aa28516a9f7c516c5cb98c1ab3fb932d709e6
Mario007s <huang75184655@163.com>
2021-05-17 15:13:24 +0800

    增加基本的 数据查询服务

--- src/main/java/mobi/xdsp/tracking/SdkApplication.java
+++ src/main/java/mobi/xdsp/tracking/SdkApplication.java
@@ -5,17 +5,28 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 @EnableScheduling
 @EnableAutoConfiguration
 @MapperScan("mobi.xdsp.tracking.mapper")
-public class SdkApplication {
+public class SdkApplication extends WebMvcConfigurerAdapter {
 
     public static void main(String[] args) {
-        System.setProperty("tomcat.util.http.parser.HttpParser.requestTargetAllow","$|{}[]()><");
+        System.setProperty("tomcat.util.http.parser.HttpParser.requestTargetAllow", "$|{}[]()><");
         System.setProperty("https.protocols", "TLSv1,TLSv1.1,TLSv1.2,TLSv3");
         SpringApplication.run(SdkApplication.class, args);
     }
 
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowCredentials(true)
+                .allowedHeaders("*")
+                .allowedOrigins("*")
+                .allowedMethods("*");
+        super.addCorsMappings(registry);
+    }
 }
