2411f4252f034847efa60441ba117e35f140514d
Mario007s <huang75184655@163.com>
2021-05-20 20:38:18 +0800

    增加基本的 数据查询服务

--- src/main/java/mobi/xdsp/tracking/router/TestingAPI.java
+++ src/main/java/mobi/xdsp/tracking/router/TestingAPI.java
@@ -32,7 +32,7 @@ import java.net.URLDecoder;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = {"p.pubearn.com","localhost.com"})
 @RestController
 public class TestingAPI {
 
