df9ca324d42ca7316594ea49ccb6cb7ed36c5373
huangyongchao <75184655@qq.com>
2021-12-26 21:04:13 +0800

    增加基本的 数据查询服务

--- src/main/java/mobi/xdsp/tracking/router/CrawlAPI.java
+++ src/main/java/mobi/xdsp/tracking/router/CrawlAPI.java
@@ -1,15 +1,22 @@
 package mobi.xdsp.tracking.router;
 
 import mobi.xdsp.tracking.common.Mailer;
+import mobi.xdsp.tracking.entity.PidMonitor;
+import mobi.xdsp.tracking.entity.PidMonitorExample;
+import mobi.xdsp.tracking.mapper.PidMonitorMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Map;
 
 @RestController
-public class NoticeAPI {
+public class CrawlAPI {
     @Autowired
     Mailer mailer;
+    @Autowired
+    PidMonitorMapper pidMonitorMapper;
 
     @RequestMapping(path = "/noticeerror", method = {RequestMethod.POST, RequestMethod.GET})
     public Object error(@RequestParam(value = "token", required = true) String token
@@ -30,6 +37,19 @@ public class NoticeAPI {
 
     ) {
 
-        return  "date" + date + " :account" + account + " :appid" + appid + " :date" + date + " :group" + group+" : "+data;
+        return "date" + date + " :account" + account + " :appid" + appid + " :date" + date + " :group" + group + " : " + data;
     }
+
+    @RequestMapping(path = "/pids", method = {RequestMethod.POST, RequestMethod.GET})
+    public Object getPidConf(@RequestParam(value = "token", required = true) String token) {
+        PidMonitorExample pidMonitorExample = new PidMonitorExample();
+        pidMonitorExample.createCriteria().andAccountIsNotNull();
+        List<PidMonitor> list = pidMonitorMapper.selectByExample(pidMonitorExample);
+        if (CollectionUtils.isEmpty(list)) {
+            return "";
+        }
+        return list;
+    }
+
+
 }
