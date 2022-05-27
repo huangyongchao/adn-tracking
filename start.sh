df91aea043915bddabee3b6c282658834bc126e0
huangyongchao <75184655@qq.com>
2022-05-27 15:58:09 +0800

    增加基本的 数据查询服务

--- start.sh
+++ start.sh
@@ -4,8 +4,6 @@ echo "start to deploy $name"
 echo "enter into workspace, " `pwd`
 git pull
 
-mvn clean install -Dmaven.test.skip=true
-
 for i in `ps -ef | grep  $name | grep java | awk '{print $2}'`
 do
   echo $i
@@ -17,6 +15,7 @@ done
 datetime=`date +%Y%m%d%H%M%S`
 echo $datetime
 
+mvn clean install -Dmaven.test.skip=true
 `mv ./target/$name*.jar  ./target/$name.jar`
 #java -server  -Xmx1g -XX:MetaspaceSize=128m  -jar  "target/$name.jar"  --spring.profiles.active=prod  &
 java -jar  -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 "target/$name.jar"  --spring.profiles.active=prod  &
